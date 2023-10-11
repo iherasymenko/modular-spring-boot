995626cddded982ad5c5415bbe97dd9b96d6f25d
Ihor Herasymenko <2904801+iherasymenko@users.noreply.github.com>
2023-10-10 21:48:33 -0400

    Fix the JUL bridge

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,8 @@ module demo.main {
     requires org.apache.tomcat.embed.core;
     requires com.fasterxml.jackson.databind;
 
+    requires jul.to.slf4j;
+
     opens com.example.demo to
             spring.core, spring.beans, spring.web, com.fasterxml.jackson.databind;
 }
