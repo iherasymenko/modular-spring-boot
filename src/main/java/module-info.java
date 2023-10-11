e4885e9d9d6c89d9652ed20c9daf601ddae2857d
Ihor Herasymenko <2904801+iherasymenko@users.noreply.github.com>
2023-10-10 21:36:06 -0400

    Fix discovering the DataSource auto-configuration
    The classloader Spring uses could not discover the `com.zaxxer.hikari.HikariDataSource` unless our module defines an explicit dependency on the module that contains this class.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,7 @@ module demo.main {
     requires spring.boot.autoconfigure;
     requires spring.boot;
 
+    requires com.zaxxer.hikari;
+
     opens com.example.demo to spring.core, spring.beans;
 }
