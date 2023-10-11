d6f1b7727850269656f1547e2a05be2d3cd08f39
Ihor Herasymenko <2904801+iherasymenko@users.noreply.github.com>
2023-10-11 00:22:50 -0400

    Clean up module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,7 @@
 module demo.main {
-    requires spring.web;
-    requires java.sql;
-    requires spring.boot.autoconfigure;
-    requires spring.boot;
 
-    requires com.zaxxer.hikari;
-
-    requires org.apache.tomcat.embed.core;
-    requires com.fasterxml.jackson.databind;
-
-    requires jul.to.slf4j;
+    requires spring.boot.starter.web;
+    requires spring.boot.starter.jdbc;
 
     opens com.example.demo to
             spring.core, spring.beans, spring.web, com.fasterxml.jackson.databind;
