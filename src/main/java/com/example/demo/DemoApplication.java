208c5d5bbbb32bce5c37c7c88110ca5f7af32b07
Ihor Herasymenko <2904801+iherasymenko@users.noreply.github.com>
2023-10-11 22:09:04 -0400

    Disable @ComponentScan
    org.springframework.core.io.support.PathMatchingResourcePatternResolver does not support "jrt:/" scanning.
    
    System.out.println(DemoApplication.class.getResource("DemoApplication.class"));
    
    When run within a jlink image, the output will be:
    jrt:/demo.main/com/example/demo/DemoApplication.class
    
    While when run on classpath/module path:
    jar:file:///<libs>/demo-0.0.1-SNAPSHOT.jar!/com/example/demo/DemoApplication.class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(proxyBeanMethods = false)
+import javax.sql.DataSource;
+
+@SpringBootConfiguration(proxyBeanMethods = false)
+@EnableAutoConfiguration
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    HelloController helloController(DataSource dataSource) {
+        return new HelloController(dataSource);
     }
 
 }
