6316ebdac76e202a7357782c4ba5e4056eb4a4e7
Ihor Herasymenko <2904801+iherasymenko@users.noreply.github.com>
2023-10-22 18:21:36 -0500

    We got ourselves a little RCE

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,9 +1,16 @@
 package com.example.demo;
 
+import org.springframework.expression.EvaluationException;
+import org.springframework.expression.ParseException;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.sql.SQLException;
 import java.util.Objects;
 
@@ -30,6 +37,19 @@ public class HelloController {
                     rs.getString(1),
                     System.getProperty("java.vendor")
             );
+        }
+    }
+
+    @PostMapping("/rce_demo")
+    public String rce(@RequestBody String body) {
+        try {
+            var parser = new SpelExpressionParser();
+            var out = parser.parseExpression(body).getValue();
+            return String.valueOf(out);
+        } catch (EvaluationException | ParseException e) {
+            var sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw, true));
+            return sw.toString();
         }
     }
 
---
+++ src/main/java/com/example/demo/SecretService.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class SecretService {
+
+    public long getSecret() {
+        return System.currentTimeMillis();
+    }
+
+}
